d78ed631b6c9fe48e60f721372d343a1ac472e35
1Tolv2 <sofiaq1@gmail.com>
2022-08-31 12:03:46 +0200

    route fixed for posts and pages

--- client/components/BlogPost.jsx
+++ client/components/BlogPost.jsx
@@ -2,10 +2,9 @@ import React from "react";
 import Link from "next/link";
 
 const BlogPost = ({ data }) => {
-  console.log("POST", data);
   return (
     <li>
-      <Link href={`/posts/${data.id}`}>
+      <Link href={`/posts/${data.slug}`}>
         <a>
           <h2>{data.title.rendered}</h2>
           <p dangerouslySetInnerHTML={{ __html: data.excerpt.rendered }}></p>
