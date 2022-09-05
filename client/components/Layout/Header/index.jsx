0387a17bd7b6cf278a3b791be1cafc8553aa5ee5
Denise Nordfalt <denisenordfalt@gmail.com>
2022-09-05 14:13:41 +0200

    removed unused code

--- client/components/Layout/Header/index.jsx
+++ client/components/Layout/Header/index.jsx
@@ -1,27 +1,6 @@
 import React, { useEffect, useState } from "react";
-import styled from "styled-components";
-
-const Container = styled.div`
-  height: 30vh;
-  width: auto;
-
-  background-color: white;
-`;
+import * as s from "./styles";
 
 export default function Header() {
-  const url = "http://localhost/wp-json/wp/v2/media/27";
-  const [data, setData] = useState([]);
-
-  useEffect(() => {
-    fetch(url, {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    })
-      .then((res) => res.json())
-      .then((data) => console.log(data));
-  }, []);
-
-  return <Container>{console.log(data)}</Container>;
+  return <s.Container></s.Container>;
 }
