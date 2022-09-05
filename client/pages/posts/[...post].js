0db25b1167c84e9bffccb4f1e9dd573c640e22f7
Denise Nordfalt <denisenordfalt@gmail.com>
2022-09-05 14:14:35 +0200

    changed to env

--- client/pages/posts/[...post].js
+++ client/pages/posts/[...post].js
@@ -21,7 +21,7 @@ export default function Post({ data }) {
 }
 
 export async function getStaticPaths() {
-  const res = await fetch(`http://localhost/wp-json/wp/v2/posts`);
+  const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/posts`);
   const data = await res.json();
   const pathList = data.map((path) => {
     return path.id.toString();
@@ -36,10 +36,12 @@ export async function getStaticProps({ params }) {
   // Ber om ursäkt this looks like shit... but it works
   const slug = params.post[0];
 
-  const pageRes = await fetch(`http://localhost/wp-json/wp/v2/posts`);
+  const pageRes = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/posts`);
   const pageData = await pageRes.json();
   const page = pageData.find((page) => page.slug === slug);
-  const res = await fetch(`http://localhost/wp-json/wp/v2/posts/${page?.id}`);
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_API_URL}/posts/${page?.id}`
+  );
   const data = await res.json();
   return {
     props: {
